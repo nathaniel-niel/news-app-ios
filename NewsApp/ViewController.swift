f4332dfe61bf6a7e6f28d8aceb4268132d5a5963
nathaniel andrian <nathaniel.task@gmail.com>
2022-12-15 11:29:33 +0700

    first commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Nathaniel Andrian on 15/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
